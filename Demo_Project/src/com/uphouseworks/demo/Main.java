29d8f2dcf1ff7445e83cb831c9538c02450c6f6e
-mxb- <mxb.atria@gmail.com>
2014-05-04 13:18:42 -0700

    added Main

---
+++ Demo_Project/src/com/uphouseworks/demo/Main.java
@@ -0,0 +1,12 @@
+package com.uphouseworks.demo;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("This is some basic java printing out");
+
+	}
+
+}
